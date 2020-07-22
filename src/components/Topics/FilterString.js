9fa2b4e1d0dd9bcc38979902435982a36d47bb8a
figmoomoo <figmoomoo@gmail.com>
2020-07-21 22:56:06 -0500

    finished assignment

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,30 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+
 
 export default class FilterString extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            names: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy'],
+            userInput: '',
+            filteredNamed: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({userInput: val});
+    }
+
     render(){
         return(
-            
+            <div className="puzzleBox filterStringPB">
+                <h4> Filter String </h4>
+                <span className="puzzleText"> Names: {JSON.stringify(this.state.names, null, 10)}</span>
+                <input className="inputLine" onChange ={(e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClick={() => this.filterNames(this.state.userInput)}></button>
+                <span className="resultsBox filterStringRB"> Filtered Names: {JSON.stringify(this.state.filteredNames, null, 10)}</span>
+            </div>
         )
     }
 }
